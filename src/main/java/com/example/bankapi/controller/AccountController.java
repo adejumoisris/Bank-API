a612366c9bd2facaca713e5294fc4d7046276148
deca <idrisadejumo@gmail.com>
2025-12-18 10:13:56 +0100

    Account Controller Created

---
+++ src/main/java/com/example/bankapi/controller/AccountController.java
@@ -0,0 +1,35 @@
+package com.example.bankapi.controller;
+
+import com.example.bankapi.model.Account;
+import com.example.bankapi.model.Transaction;
+import com.example.bankapi.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/accounts")
+@RequiredArgsConstructor
+public class AccountController {
+    private final AccountService accountService;
+
+    @GetMapping("/{accountNumber}/balance")
+    public BigDecimal getBalance(@PathVariable String accountNumber) {
+        return accountService.getBalance(accountNumber);
+    }
+
+    @GetMapping("/{accountNumber}")
+    public Account getAccountDetails(@PathVariable String accountNumber) {
+        return accountService.getAccountDetails(accountNumber);
+    }
+    @GetMapping("/{accountNumber}/transaction")
+    public List<Transaction> getTransactions(@PathVariable String accountNumber) {
+        return accountService.getTransactionsHistory(accountNumber);
+
+    }
+}
