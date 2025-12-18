491d89223706525d80b5b289b32d6f67198493a6
deca <idrisadejumo@gmail.com>
2025-12-18 09:22:06 +0100

    account created

--- src/main/java/com/example/bankapi/model/Account.java
+++ src/main/java/com/example/bankapi/model/Account.java
@@ -1,4 +1,114 @@
 package com.example.bankapi.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String accountNumber;
+    private String accountName;
+    private BigDecimal balance;
+    private String accountType;
+    private String currency;
+    private String status;
+    private LocalDateTime createdAt;
+
+    public Account(Long id, String accountNumber, String accountName, BigDecimal balance, String accountType, String currency, String status, LocalDateTime createdAt) {
+        this.id = id;
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+        this.balance = balance;
+        this.accountType = accountType;
+        this.currency = currency;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+
+    public Account() {
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", accountNumber='" + accountNumber + '\'' +
+                ", accountName='" + accountName + '\'' +
+                ", balance=" + balance +
+                ", accountType='" + accountType + '\'' +
+                ", currency='" + currency + '\'' +
+                ", status='" + status + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 }
